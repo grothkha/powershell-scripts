4fce414517caaad43bd4022b2a3188b0ea671570
Hannes <korporalnobbs@gmail.com>
2018-11-30 17:03:06 +0100

    comments

--- toggleRefreshRate.ps1
+++ toggleRefreshRate.ps1
@@ -2,7 +2,9 @@
 # Description: Script to toggle monitor refresh rate between two hardcoded values.
 # Author: Hannes Grothkopf
 # TODOs: Better rate setting handling, make monitor ID configurable
-# Info: https://docs.microsoft.com/en-us/powershell/scripting/getting-started/cookbooks/getting-wmi-objects--get-wmiobject-?view=powershell-6
+# Info: 
+# - https://docs.microsoft.com/en-us/powershell/scripting/getting-started/cookbooks/getting-wmi-objects--get-wmiobject-?view=powershell-6
+# - https://superuser.com/questions/1184815/is-a-way-to-quickly-set-screen-refresh-rate
 ####
 
 $RefreshRate1 = 60
